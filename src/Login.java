d18539eb4df5040d77b29df5ba9d612c88350281
ANSHTHAKURR <suryanshjaryal114@gmail.com>
2024-03-02 16:13:02 +0530

    bank management

---
+++ src/Login.java
@@ -0,0 +1,115 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Login extends JFrame implements ActionListener {
+    JLabel label1, label2, label3;
+    JTextField textField2;
+    JPasswordField jPasswordField3;
+    JButton button1, button2, button3;
+
+    public Login() {
+        super("BANK MANAGEMENT SYSTEM");
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icon/bank.png"));
+        Image i2 = i1.getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        image.setBounds(220, 4, 100, 100);
+        add(image);
+
+        ImageIcon ii1 = new ImageIcon(ClassLoader.getSystemResource("icon/card.png"));
+        Image ii2 = ii1.getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT);
+        ImageIcon ii3 = new ImageIcon(ii2);
+        JLabel iimage = new JLabel(ii3);
+        iimage.setBounds(430, 250, 100, 100);
+        add(iimage);
+
+        label1 = new JLabel("WELCOME TO ATM");
+        label1.setForeground(Color.WHITE);
+        label1.setFont(new Font("AvantGarde", Font.BOLD, 38));
+        label1.setBounds(110, 100, 450, 40);
+        add(label1);
+
+
+        label2 = new JLabel("CARD NO:");
+        label2.setFont(new Font("Ralway", Font.BOLD, 28));
+        label2.setForeground(Color.WHITE);
+        label2.setBounds(40, 180, 375, 25);
+        add(label2);
+
+        textField2 = new JTextField(15);
+        textField2.setBounds(170, 180, 350, 25);
+        textField2.setFont(new Font("Arial", Font.BOLD, 14));
+        add(textField2);
+
+
+        label3 = new JLabel("PIN No:");
+        label3.setFont(new Font("Ralway", Font.BOLD, 28));
+        label3.setForeground(Color.WHITE);
+        label3.setBounds(60, 210, 375, 25);
+        add(label3);
+
+
+        jPasswordField3 = new JPasswordField(15);
+        jPasswordField3.setBounds(170, 210, 350, 25);
+        jPasswordField3.setFont(new Font("Arial", Font.BOLD, 14));
+        add(jPasswordField3);
+
+
+        button1 = new JButton("SIGN IN");
+        button1.setFont(new Font("Arial", Font.BOLD, 14));
+        button1.setForeground(Color.black);
+        button1.setBounds(170, 250, 100, 30);
+        button1.addActionListener(this);
+        add(button1);
+
+        button2 = new JButton("CLEAR");
+        button2.setFont(new Font("Arial", Font.BOLD, 14));
+        button2.setForeground(Color.black);
+        button2.setBounds(270, 250, 100, 30);
+        button2.addActionListener(this);
+        add(button2);
+
+        button3 = new JButton("SIGN UP");
+        button3.setFont(new Font("Arial", Font.BOLD, 14));
+        button3.setForeground(Color.black);
+        button3.setBounds(170, 280, 200, 30);
+        button3.addActionListener(this);
+        add(button3);
+
+        ImageIcon iii1 = new ImageIcon(ClassLoader.getSystemResource("icon/backbg.png"));
+        Image iii2 = iii1.getImage().getScaledInstance(550, 380, Image.SCALE_DEFAULT);
+        ImageIcon iii3 = new ImageIcon(iii2);
+        JLabel iiimage = new JLabel(iii3);
+        iiimage.setBounds(0, 0, 550, 380);
+        add(iiimage);
+
+
+        setLayout(null);
+        setSize(550, 380);
+        setVisible(true);
+        setLocation(350, 200);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        try {
+            if (e.getSource() == button1) {
+
+            } else if (e.getSource() == button2) {
+
+                textField2.setText("");
+                jPasswordField3.setText("");
+            } else if (e.getSource() == button3) {
+
+            }
+        }catch (Exception E){
+            E.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new Login();
+    }
+}
